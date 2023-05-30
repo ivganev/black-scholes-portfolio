133ac54f7bde77742df092797d3a3adafba09925
Iordan Ganev <44288642+ivganev@users.noreply.github.com>
2023-05-30 11:32:01 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use statrs::distribution::Normal;
 use statrs::distribution::Continuous;
 use statrs::distribution::ContinuousCDF;
+use core::num;
 use std::collections::HashMap;
 
 #[allow(dead_code)]
@@ -18,6 +19,7 @@ struct Portfolio {
     contracts: f32,
     options: Vec<Opt>,
     cash: f32,
+    time_to_expiry: f32,
 }
 
 enum OptKind {
@@ -37,6 +39,68 @@ struct Opt {
 impl Portfolio {
     fn underlying_summary(&self) -> String {
         return format!("The underlying has current price {} with volatiilty {}.", self.spot, self.volatility);
+    }
+
+    fn likely_underlying_range(&self) -> [f32; 2] {
+        let mut lower = 0.0;
+        if self.volatility < 1.0/3.0 {
+            lower = self.spot*(1.0 - 3.0*self.volatility);
+        }
+        return [lower, self.spot*(1.0  + 3.0*self.volatility)];
+    }
+
+    fn add_option(&mut self, op: Opt) {
+        if op.time_to_expiry > self.time_to_expiry {
+            self.time_to_expiry = op.time_to_expiry;
+        }
+        self.options.push(op);
+    }
+
+    fn price_changes(&self, number_samples: i32) -> Vec<Vec<f64>> {
+        let [lower, upper] = self.likely_underlying_range();
+        let increment_size = (upper - lower)/(number_samples as f32);
+
+        let mut v: Vec<Vec<f64>> = Vec::new();
+
+        for i in 0..(number_samples) {
+            v.push(vec![(lower + (i as f32)*increment_size) as f64, 0f64]);
+        }
+
+        let r = self.interest_rate as f64;
+        let sigma = self.volatility as f64;
+        let n = Normal::new(0.0, 1.0).unwrap();
+
+        for op in &self.options {
+            let t =  op.time_to_expiry as f64;
+            let k = op.strike_price as f64;
+            let q = op.quantity as f64;
+
+            for i in 0..(number_samples) {
+                let x = (lower + (i as f32)*increment_size) as f64;
+                let dplus = ((x/k).ln() +  (r + sigma*sigma*0.5)*t )/(sigma*(t.sqrt()));
+                let dminus = ((x/k).ln() +  (r - sigma*sigma*0.5)*t )/(sigma*(t.sqrt()));
+                match op.kind {
+                    OptKind::Call => {
+                        v[i as usize][1] += q*(x*n.cdf(dplus as f64) - ((-r*t).exp())*k*n.cdf(dminus as f64));
+                    },
+                    OptKind::Put => {
+                        v[i as usize][1] += q*(-x*n.cdf(-dplus as f64) + ((-r*t).exp())*k*n.cdf(-dminus as f64));            
+                    },
+                }
+            }
+        }
+
+        let mut price_range = vec![0f64, 0f64];
+        for i in 0..(number_samples) {
+            if v[i as usize][1] < price_range[0] {
+                price_range[0] = v[i as usize][1];
+            }
+            if v[i as usize][1] > price_range[1] {
+                price_range[1] = v[i as usize][1]
+            }
+        }
+
+        return v; 
     }
 
     fn summary(&self) {
@@ -66,6 +130,8 @@ impl Portfolio {
         let mut theta = 0.0;
         let mut vega = 0.0;
         let mut rho = 0.0;
+        let n = Normal::new(0.0, 1.0).unwrap();
+
         for op in &self.options {
             let t =  op.time_to_expiry as f64;
             let k = op.strike_price as f64;
@@ -74,7 +140,6 @@ impl Portfolio {
             let dplus = ((x/k).ln() +  (r + sigma*sigma*0.5)*t )/(sigma*(t.sqrt()));
             let dminus = ((x/k).ln() +  (r - sigma*sigma*0.5)*t )/(sigma*(t.sqrt()));
 
-            let n = Normal::new(0.0, 1.0).unwrap();
             gamma += q*n.pdf(dplus as f64)/(x*sigma*t.sqrt());
             vega += q*x*n.pdf(dplus as f64)*t.sqrt();
 
@@ -120,6 +185,7 @@ fn main() {
         options: Vec::new(),
         cash: 0.0,
         interest_rate: 0.01,
+        time_to_expiry: 0.0,
     };
 
     stock1.options.push( Opt {
@@ -142,7 +208,7 @@ fn main() {
         time_to_expiry: 1.0,
         quantity: 1.0
     });
-    
+
     println!("{}", stock1.underlying_summary());
     println!("{:?}", stock1.greeks());
     stock1.summary();
@@ -154,42 +220,13 @@ fn main() {
         contracts:  7.0,
         options: Vec::new(),
         cash: 0.0,
+        time_to_expiry: 0.0,
     };
 
     porfolio1.options.push( Opt { 
         kind: OptKind::Call, strike_price: 95.0, time_to_expiry: 91.0/365.0, quantity: -10.0 
     });
     println!("{:?}", porfolio1.greeks());
-
-
-    ///////////////
-
-
-    let interest_rate1 = InterestRate {
-        rate: 0.01,
-    };
-
-    println!("{}", interest_rate1.summary());
-    //println!("{}", option1.summary());
-//    println!("{}", option2.summary());
-
-    for i in 0..4 {
-        let h = call_greeks(1.0, (80 + 5*i) as f32, 100.0, 0.3, 0.01 );
-        //println!("The greeks of the {} call are: {:?}", 80+5*i, h);
-        //if let Some(p) = h.get("theta") {   println!("The theta of the {} call is {}", 80 + 5*i, p);  }
-    }
-
-    /* for i in 0..4 {
-        let h = put_greeks(1.0, (80 + 5*i) as f32, 100.0, 0.3, 0.01 );
-        if let Some(p) = h.get("price") {
-            println!("The price of the put is {}", p);
-        }
-    }
-    */
-
-   // println!("{}", bsm(&option2, &stock1, &interest_rate1));
-
-
 
 }
 
@@ -198,6 +235,25 @@ fn main() {
 ///////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////
+/// 
+/// 
+
+fn call_price(t: f32, x: f32, k: f32, sigma: f32, r: f32) -> f64 {
+    let dplus = ((x/k).ln() +  (r + sigma*sigma*0.5)*t )/(sigma*(t.sqrt()));
+    let dminus = ((x/k).ln() +  (r - sigma*sigma*0.5)*t )/(sigma*(t.sqrt()));
+
+    let t = t as f64;
+    let x = x as f64;
+    let k = k as f64;
+    let sigma = sigma as f64;
+    let r = r as f64;
+
+    let n = Normal::new(0.0, 1.0).unwrap();
+    return x*n.cdf(dplus as f64) - ((-r*t).exp())*k*n.cdf(dminus as f64);
+}
+
+
+
 
 fn call_greeks(t: f32, x: f32, k: f32, sigma: f32, r: f32) -> HashMap<String, f64> {
     let dplus = ((x/k).ln() +  (r + sigma*sigma*0.5)*t )/(sigma*(t.sqrt()));
@@ -278,48 +334,3 @@ impl Opt {
         }
     }
 }
-
-
-//////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////// 
-/*
-fn bsm(option: &Opt, stock: &Underlying, interest: &InterestRate) -> f64 {
-    // x F(d_+) + K e^{-rt} F(d_-)
-    let sigma = stock.volatility as f64;
-    let x = stock.spot as f64;
-    let k = option.strike_price as f64;
-    let t = option.time_to_expiry as f64;
-    let r = interest.rate as f64;
-    let dplus = ((x/k).ln() +  (r + sigma*sigma*0.5)*t )/(sigma*(t.sqrt()));
-    let dminus = ((x/k).ln() +  (r - sigma*sigma*0.5)*t )/(sigma*(t.sqrt()));
-
-    let n = Normal::new(0.0, 1.0).unwrap();
-    return x*n.cdf(dplus) - ((-r*t).exp())*k*n.cdf(dminus);
-}
-
-fn call_price(t: f32, x: f32, k: f32, sigma: f32, r: f32) -> f64 {
-    let dplus = ((x/k).ln() +  (r + sigma*sigma*0.5)*t )/(sigma*(t.sqrt()));
-    let dminus = ((x/k).ln() +  (r - sigma*sigma*0.5)*t )/(sigma*(t.sqrt()));
-
-    let n = Normal::new(0.0, 1.0).unwrap();
-    return (x as f64)*n.cdf(dplus as f64) - ((-r*t).exp() as f64)*(k as f64)*n.cdf(dminus as f64);
-}
-
-fn call_delta(t: f32, x: f32, k: f32, sigma: f32, r: f32) -> f64 {
-    let dplus = ((x/k).ln() +  (r + sigma*sigma*0.5)*t )/(sigma*(t.sqrt()));
-
-    let n = Normal::new(0.0, 1.0).unwrap();
-    return n.cdf(dplus as f64);
-}
-
-fn put_price(t: f32, x: f32, k: f32, sigma: f32, r: f32) -> f64 {
-    let dplus = ((x/k).ln() +  (r + sigma*sigma*0.5)*t )/(sigma*(t.sqrt()));
-    let dminus = ((x/k).ln() +  (r - sigma*sigma*0.5)*t )/(sigma*(t.sqrt()));
-
-    let n = Normal::new(0.0, 1.0).unwrap();
-    return -(x as f64)*n.cdf(-dplus as f64) + ((-r*t).exp() as f64)*(k as f64)*n.cdf(-dminus as f64);
-}
-
-*/
